70cd4c3e8449802c230daa710524744ca1c462a8
HardfeigN <yarik.yesip@gmail.com>
2024-04-02 15:50:12 +0700

    Middleware was added

---
+++ ProjectDash.Backend/ProjectDash.Web/Middleware/CustomExceprtionHandlerMiddleware.cs
@@ -0,0 +1,52 @@
+﻿using FluentValidation;
+using ProjectDash.Application.Common.Exceptions;
+using System.Net;
+using System.Text.Json;
+
+namespace ProjectDash.Web.Middleware
+{
+    public class CustomExceprtionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public CustomExceprtionHandlerMiddleware(RequestDelegate next) =>
+            _next = next;
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception exception) 
+            {
+                await HandleExceptionAsync(context, exception);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            var code = HttpStatusCode.InternalServerError;
+            var result = string.Empty;
+            switch (exception)
+            {
+                case ValidationException validationException:
+                    code = HttpStatusCode.BadRequest;
+                    result = JsonSerializer.Serialize(validationException.Errors);
+                    break;
+                case NotFoundException:
+                    code = HttpStatusCode.NotFound;
+                    break;
+            }
+
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            if (result == string.Empty)
+            {
+                result = JsonSerializer.Serialize(new { error = exception.Message });
+            }
+
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
---
+++ ProjectDash.Backend/ProjectDash.Web/Middleware/CustomExceprtionHandlerMiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace ProjectDash.Web.Middleware
+{
+    public static class CustomExceprtionHandlerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomExceprtionHandler(this IApplicationBuilder applicationBuilder)
+        {
+            return applicationBuilder.UseMiddleware<CustomExceprtionHandlerMiddleware>();
+        }
+    }
+}
